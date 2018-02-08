b2eca74256ae641152fef99fc0185436c71633b3
Ivar haugerud <ivarhaugerud@Ivars-MacBook-Pro.local>
2018-02-08 23:24:18 +0100

    almost finished

--- rapport1_StrømOgSpenning/plotting/plotting_data.py
+++ rapport1_StrømOgSpenning/plotting/plotting_data.py
@@ -4,6 +4,19 @@ import seaborn as sns
 sns.set_style("whitegrid")
 blue, = sns.color_palette("muted", 1)
 
+def function(x,y):
+    n=len(x)
+    D=(sum(x**2))-(1/n)*(((sum(x)))**2)
+    E=np.sum((x*y)) - (1/n)*np.sum(x)*np.sum(y)
+    
+    F=sum(y**2)-(1/n)*sum(y**2)**2
+    m=E/D
+    c=np.mean(y)-m*np.mean(x);
+    delta_m= np.sqrt((1/(n-2))*(D*F-E**2)/(D**2))
+    print(D)
+    delta_c= np.sqrt((1/(n-2))*((D/n)+np.mean(x)**2)*(D*F-E**2)/(D**2))
+    return m,c,delta_m,delta_c
+
 freq_A = np.array([10, 100, 1000, 10000, 100000, int(1e6)  , 500, 5000, 50000]) #Hz
 freq_B =np.array([10, 100, 1000, 10000, 100000, int(6*1e6), 500, 5000, 50000]) #Hz
 amplitude_A = np.array([706.5, 695.5, 667.2, 664.9, 657.6, 603, 669.9, 665, 664.5])*1e-3 #volt
@@ -17,9 +30,9 @@ measurments_number = np.linspace(1, len(freq_A), len(freq_A))
 log_y = np.log10(amplitude_B[-8:]/amplitude_A[-8:])
 log_x = np.log10(freq_A[-8:])
 
-[b, a] = np.polyfit(log_x, log_y, 1)
-poly_foo = 10**(a)* 10**(b*log_x)
-print(b)
+[a, b, delta_a, delta_b] = function(log_x, log_y)
+poly_foo = 10**(b)* 10**(a*log_x)
+print(a, b, delta_a, delta_b)
 fig = plt.figure(figsize=(7.2, 7.2), dpi=100)
 font = {"size"  : 14}
 plt.rc('font', **font)
@@ -27,8 +40,10 @@ ax = fig.add_subplot(111)
 ax.set_yscale('log')
 ax.set_xscale('log')
 ax.plot(10**(log_x), poly_foo, "--")
-ax.errorbar(freq_A, amplitude_B/amplitude_A, yerr=sigma_av+sigma_bv, xerr=sigma_fa, fmt='o', ecolor='g', capthick=2)
+ax.plot(freq_A, amplitude_B/amplitude_A, "bo")
 plt.show()
+y_0 = -b/a
+print(y_0)
 """
 def uncertenty_F75(x):
     error = np.zeros(len(x))

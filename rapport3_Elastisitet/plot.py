01814ac201bf14f245ee625cc52d11f416e9c62a
Ivar haugerud <ivarhaugerud@1x-193-157-194-25.uio.no>
2018-04-02 19:29:53 +0200

    more stuff

--- rapport3_Elastisitet/plot.py
+++ rapport3_Elastisitet/plot.py
@@ -7,7 +7,9 @@ blue, = sns.color_palette("muted", 1)
 
 vekt = np.array([0, 500.15, 1000.30, 1500.2, 2000.1, 2499.7, 3000.5, 3500.65])*1e-3 #kg
 utslag  = (np.array([8.18, 7.45, 6.71, 6.03, 5.23, 4.49, 3.75, 2.99])-8.18)*1e-3 #m
-
+usikkerhet = np.array([0, 1, 1, 1, 2, 3, 3, 3])*1e-5
+print(utslag)
+print(usikkerhet)
 [a, b] = np.polyfit(vekt, utslag, 1)
 print(a, b)
 fig = plt.figure(figsize=(7.0, 7.2), dpi=100)
@@ -16,7 +18,7 @@ plt.rc('font', **font)
 ax = fig.add_subplot(111)
 fig.suptitle(r"Nedbøying av messingstav som funksjon av vekt", fontsize=17, fontweight='bold')
 plt.plot(vekt, b+vekt*a)
-plt.plot(vekt, utslag, 'ro')
+plt.errorbar(vekt, utslag, yerr=usikkerhet)
 ax.set_xlabel("Vekt [kg]", fontsize=14)
 ax.set_ylabel("Nedbøying [cm]", fontsize=14)
 plt.savefig("pic/nedbojing.pdf")
--- rapport3_Elastisitet/plotting.m
+++ rapport3_Elastisitet/plotting.m
@@ -1,3 +1,4 @@
+%{
 vekt = linspace(0, 3.5, 8);
 utslag = [8.18, 7.45, 6.71, 6.03, 5.23, 4.49, 3.75, 2.99]-8.18;
 [m, c, delta_c, delta_m] = function1(vekt, utslag)
@@ -8,3 +9,40 @@ hold on
 xlabel('vekt [kg]')
 ylabel('utslag [mm]')
 plot(vekt, c+m*vekt)
+%}
+
+figure(1)
+scatter(fut, energi/max(energi), 15, 'filled')
+hold on
+xlabel('Frekvens [hz]','fontsize',16)
+ylabel('Relativ energi','fontsize',16)
+title('Fouriertransformerte av signalet','fontsize',16)
+box on
+axis([0 1500 -0.05 1.05])
+grid on
+plot(fut, energi/max(energi))
+
+%figure(2)
+%plot(t, data)
+
+
+
+[c index] = min(abs(fut-1207.2))
+%figure(3)
+index = index -1 ;
+intervall= 3;
+x2 = fut(1, index-intervall:index+intervall);
+y2 = energi(index-intervall:index+intervall, 1);
+
+figure(1)
+% create smaller axes in top right, and plot on it
+axes('Position',[0.39 0.37 0.26 0.445])
+box on
+scatter(x2, y2/max(y2), 15, 'filled')
+ax = gca;
+axis([min(x2) max(x2) -0.05 1.05])
+hold on
+box on
+plot(x2, y2/max(y2))
+
+

b0c52bba86fb093c66f651f2bbd7a0cb25680352
Ivar haugerud <ivarhaugerud@Ivars-MacBook-Pro.local>
2018-04-02 09:23:29 +0200

    more latex

--- rapport3_Elastisitet/plot.py
+++ rapport3_Elastisitet/plot.py
@@ -1,5 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+blue, = sns.color_palette("muted", 1)
 
-vekt = np.linspace(0, 3.5, 8)
-utslag  
+
+vekt = np.array([0, 500.15, 1000.30, 1500.2, 2000.1, 2499.7, 3000.5, 3500.65])*1e-3 #kg
+utslag  = (np.array([8.18, 7.45, 6.71, 6.03, 5.23, 4.49, 3.75, 2.99])-8.18)*1e-3 #m
+
+[a, b] = np.polyfit(vekt, utslag, 1)
+print(a, b)
+fig = plt.figure(figsize=(7.0, 7.2), dpi=100)
+font = {"size"  : 14}
+plt.rc('font', **font)
+ax = fig.add_subplot(111)
+fig.suptitle(r"Nedbøying av messingstav som funksjon av vekt", fontsize=17, fontweight='bold')
+plt.plot(vekt, b+vekt*a)
+plt.plot(vekt, utslag, 'ro')
+ax.set_xlabel("Vekt [kg]", fontsize=14)
+ax.set_ylabel("Nedbøying [cm]", fontsize=14)
+plt.savefig("pic/nedbojing.pdf")
+plt.show()

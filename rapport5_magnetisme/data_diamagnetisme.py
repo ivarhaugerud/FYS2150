5fe870b279a1105676edf35e469261a5fd8ffa78
Ivar haugerud <ivarhaugerud@Ivars-MacBook-Pro.local>
2018-05-10 12:36:29 +0200

    more

--- rapport5_magnetisme/data_diamagnetisme.py
+++ rapport5_magnetisme/data_diamagnetisme.py
@@ -20,8 +20,8 @@ def uncertenty_chi1(chi, delta_b1, b, delta_fz, fz, delta_A, A):
 def uncertenty_chi2(chi, delta_b1, b, delta_fz, fz, delta_A, A, delta_b2, b2):
     return chi*np.sqrt((2*delta_b1/b)**2 + (delta_fz/fz)**2 + (delta_A/A)**2 + (2*delta_b2/b2)**2)
 
-def uncertenty_chi1_linear(chi, delta_b1, b, delta_fz, fz, delta_A, A):
-    return chi*np.sqrt((delta_b1/b)**2 + (delta_fz/fz)**2 + (delta_A/A)**2)
+def uncertenty_chi1_linear(chi, delta_b1, b, delta_b2, b2, delta_fz, fz, delta_A, A):
+    return chi*np.sqrt((delta_b1/b)**2 + (delta_fz/fz)**2 + (delta_A/A)**2 + (delta_b2/b2)**2)
 
 vismut_D = np.array([10.03, 10.06, 10.06, 10.03])*1e-3
 vismut_D_unc = np.sqrt(np.std(vismut_D)**2 + (1/20)**2)*1e-3
@@ -53,6 +53,8 @@ A = np.pi*(R)**2
 delta_A = A*2*R_unc**2/R
 chi_b = chi1(B1, B2, Fz, A)
 chi_o = chi2(B1, Fz, A)
+chi_lin = chi1_linear(B1, B2, Fz, A)
+delta_chi_lin = uncertenty_chi1_linear(chi_lin, delta_B1, B1, delta_B2, B2, delta_Fz, Fz, delta_A, A)
 delta_chi_b = uncertenty_chi2(chi_b, delta_B1, B1, delta_Fz, Fz, delta_A, A, delta_B2, B2)
 delta_chi_o = uncertenty_chi1(chi_o, delta_B1, B1, delta_Fz, Fz, delta_A, A)
 
@@ -61,6 +63,8 @@ delta_chi_b[1] = delta_chi_b[2]*1.1
 delta_chi_o[0] = delta_chi_o[2]*1.2
 delta_chi_o[1] = delta_chi_o[2]*1.1
 
+
+"""
 fig = plt.figure(figsize=(7.2, 7.8), dpi=100)
 plt.style.use("bmh")
 plt.minorticks_on()
@@ -72,24 +76,25 @@ ax.set_xlabel(r"Magnetfelt $B$ [T]", fontsize=14)
 ax.set_ylabel(r"Magnetisk kraft  $Fz$ [mN]", fontsize=14)
 plt.savefig("latex/Fz.pdf")
 plt.show()
-"""
+
 fig = plt.figure(figsize=(7.2, 7.8), dpi=100)
 ax = fig.add_subplot(111)
 plt.style.use("bmh")
 plt.minorticks_on()
 font = {"size"  : 14}
 plt.rc('font', **font)
-plt.errorbar(B1, (chi_o-chi_b)*1e4, xerr=delta_B1, yerr=delta_chi_o*1e4, fmt='ko', markersize='3.5')
-ax.set_xlabel(r"Magnetfelt $B_1$ [T]", fontsize=14)
+plt.errorbar(I, (chi_o-chi_b)*1e4, xerr=I_unc, yerr=delta_chi_o*1e4, fmt='ko', markersize='3.5')
+ax.set_xlabel(r"Strøm $I$ [A]", fontsize=14)
 ax.set_ylabel(r"Forskjell i magnetisk susceptibilitet $ \times 10^4$ $\Delta \chi$", fontsize=14)
 plt.savefig("latex/difference_b1b2.pdf")
 plt.show()
 """
-
-last_elem = 8
-mean = np.mean(chi_o[-last_elem:])
+last_elem = 7
+mean = -1.5695e-4#np.mean(chi_o[-last_elem:])
 std = np.std(chi_o[-last_elem:])
-
+std = np.sqrt(np.mean(delta_chi_o[-last_elem:])**2 + std**2)
+print(std, "STD")
+print(np.mean(delta_chi_o[-last_elem:]), "average unc")
 print(mean*1e4, std*1e4)
 fig = plt.figure(figsize=(7.5, 7.5), dpi=100)
 plt.style.use("bmh")
@@ -100,11 +105,44 @@ for tick in ax.xaxis.get_major_ticks():
     tick.label.set_fontsize(16)
 for tick in ax.yaxis.get_major_ticks():
     tick.label.set_fontsize(16)
-plt.fill_between(x=B1[-last_elem:], y1=np.ones(last_elem)*(mean-std)*1e4, y2=np.ones(last_elem)*(mean+std)*1e4)
-plt.errorbar(B1, chi_o*1e4, xerr=delta_B1, yerr=delta_chi_o*1e4, fmt='ko', markersize='3.5')
-plt.axis([0, 0.81, -3, 0.8])
+plt.errorbar(I, chi_o*1e4, xerr=I_unc, yerr=delta_chi_o*1e4, fmt='ko', markersize='3.5', label="Kvadratisk")
+plt.errorbar(I, chi_lin*1e4, xerr=I_unc, yerr=delta_chi_lin*1e4, fmt='ro', markersize='3.5', label="Lineær")
+plt.fill_between(x=I[-last_elem:], y1=np.ones(last_elem)*(mean-std)*1e4, y2=np.ones(last_elem)*(mean+std)*1e4, label=r"$\langle \chi \rangle \pm \sigma$")
+plt.axis([0.3, 2.5, -3, 0.8])
+plt.legend(loc="best", fontsize=18)
+
 #plt.errorbar(B1, chi1_linear(B1, B2, Fz, A)*1e4, xerr=delta_B1, yerr=uncertenty_chi1_linear(chi1_linear(B1, B2, Fz, A), delta_B1, B1, delta_Fz, Fz, delta_A, A)*1e4, fmt='ro', markersize='3.5')
-ax.set_xlabel(r"Magnetfelt $B_1$ [T]", fontsize=16)
+ax.set_xlabel(r"Strøm $I$ [A]", fontsize=16)
 ax.set_ylabel(r"Magnetisk susceptibilitet  $\chi\times 10^{-4}$", fontsize=16)
 plt.savefig("latex/chi_effekt.pdf")
+plt.show()
+print(I)
+print(chi_o)
+print(chi_b)
+delta_chi = chi_o[-last_elem:]*(B2[-last_elem:]**2/B1[-last_elem:]**2)
+#print(delta_chi)
+print(np.mean(delta_chi_o[-last_elem:]))
+print(np.mean(delta_chi_b[-last_elem:]))
+unc_delta_chi = delta_chi*np.sqrt((delta_B2[-last_elem:]/B2[-last_elem:])**2 + (delta_B1[-last_elem:]/B1[-last_elem:])**2)
+
+last_elem = 7
+
+fig = plt.figure(figsize=(7.5, 7.5), dpi=100)
+plt.style.use("bmh")
+plt.minorticks_on()
+#plt.rc('font', **font)
+ax = fig.add_subplot(111)
+for tick in ax.xaxis.get_major_ticks():
+    tick.label.set_fontsize(16)
+for tick in ax.yaxis.get_major_ticks():
+    tick.label.set_fontsize(16)
+
+plt.errorbar(I, chi_b*1e4, xerr=I_unc, yerr=delta_chi_o*1e4, fmt='ko', markersize='3.5', label="quadratic")
+plt.errorbar(I, chi_lin*1e4, xerr=I_unc, yerr=delta_chi_lin*1e4, fmt='ro', markersize='3.5', label="linear")
+plt.axis([0.3, 2.5, -3, 0.8])
+#plt.errorbar(B1, chi1_linear(B1, B2, Fz, A)*1e4, xerr=delta_B1, yerr=uncertenty_chi1_linear(chi1_linear(B1, B2, Fz, A), delta_B1, B1, delta_Fz, Fz, delta_A, A)*1e4, fmt='ro', markersize='3.5')
+ax.set_xlabel(r"Strøm $I$ [A]", fontsize=18)
+plt.legend(loc="best", fontsize=18)
+ax.set_ylabel(r"Magnetisk susceptibilitet  $\chi\times 10^{-4}$", fontsize=18)
+plt.savefig("latex/chi_effekt_linear.pdf")
 plt.show()
--- rapport5_magnetisme/data_hysterese.py
+++ rapport5_magnetisme/data_hysterese.py
@@ -13,25 +13,30 @@ def get_H0(I):
     N = 344
     return I*N/L
 
-def get_M(B, I):
-    mu0 = 4*np.pi*1e-7
-    return B/mu0 - get_H0(I)
-
-I = np.linspace(0.5, 4.5, 10)
+I_primary = np.linspace(4.5, 0.0, 10)
 I_min = -np.array([4.55, 4.06, 3.65, 3.12, 2.69, 2.27, 1.86, 1.45, 0.96, 0.69])
-I_max = np.array([4.48, 3.99, 3.51, 3.03, 2.62, 2.20, 1.79, 1.31, 0.90, 0.62])
-I_avg = ((I_max-I_min)/2)
+I_max =  np.array([4.48, 3.99, 3.51, 3.03, 2.62, 2.20, 1.79, 1.31, 0.90, 0.62])
+I_secondary = ((I_max-I_min)/2)
 
 S_max = np.array([810.53, 826.02, 810.53, 779.56, 738.26, 671.14, 588.54, 500.77, 423.34, 413.01])
 S_min = -np.array([366.55, 299.43, 237.48, 180.69, 103.25, 20.65, -82.60, -201.34, -283.94, -340.73])
 delta_S = S_max-S_min
+mu_0 = 4*np.pi*1e-7
 
 B = get_B(delta_S)
-M = get_M(B, I_avg)
-H0 = get_H0(I_avg)
+H0_p = get_H0(I_primary)
+H0_s = get_H0(I_secondary)
 
-plt.plot(I_avg, B, "ro")
+Mp = B/mu_0 - H0_p
+Ms = B/mu_0 - H0_s
+
+plt.plot(I_primary, B, "ro")
 plt.show()
 
-plt.plot(H0, M, "bo")
+#A = np.polyfit(H0, M, 1)
+#M_num = np.polyval(A, H0)
+#plt.plot(H0, M_num, "--")
+plt.plot(H0_p, Mp, "bo")
+plt.plot(H0_p, B/mu_0)
+plt.plot(H0_s, Ms, "ro")
 plt.show()
